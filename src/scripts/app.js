ecc01acb03e52beea62b6e3174ef8760c7fa2104
Wenzgate <cornelis.m@outlook.com>
2023-05-29 22:51:53 +0200

    Adding full SCSS and BurgerMenu Animation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,27 @@
 import { gsap } from "gsap";
-console.log(gasp.version);
+console.log(gsap.version);
+
+
+const openMenu = document.querySelector('.navigation__btn--open');
+const closeMenu = document.querySelector('.navigation__btn--close');
+
+openMenu.addEventListener('click', () => {
+    document.querySelector('.navigation__menu').classList.add('navigation__menu--open');
+})
+
+closeMenu.addEventListener('click', () => {
+    document.querySelector('.navigation__menu').classList.remove('navigation__menu--open');
+})
+
+
+
+//Redimensionnent du viweport
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+//Redimensionnent du viweport
+
 
